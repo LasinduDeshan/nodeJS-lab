abea0c82fc5c09eacb342f94e8bb96d2c772b437
Lasindu Deshan <141449113+LasinduDeshan@users.noreply.github.com>
2026-02-23 22:01:09 +0530

    c

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const myPromise = Promise.resolve("Success with Async!");
-
-async function myFunction() {
-  try {
-    const result = await myPromise;
-    console.log(result);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-myFunction();
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
