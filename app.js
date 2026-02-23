78b6b3d2bb36aca17a6c02ee8a31cb663678b687
Lasindu Deshan <141449113+LasinduDeshan@users.noreply.github.com>
2026-02-23 22:06:57 +0530

    c

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello GitHub Feature Branch!");
+const myPromise = Promise.resolve("Success with Async!");
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
